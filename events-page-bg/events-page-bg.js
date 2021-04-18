79a5cfde82191d671b7b0ca35c6c033a79c25355
Ege Uz <enkomaun@gmail.com>
2021-04-18 20:17:04 +0300

    event bg cleanup

--- events-page-bg/events-page-bg.js
+++ events-page-bg/events-page-bg.js
@@ -3,22 +3,19 @@ const canvasBG = document.createElement("div")
 canvasBG.id = "canvas-bg"
 canvasBG.style.width = "100%"
 canvasBG.style.height = "100%"
-// canvasBG.style.backgroundColor = "blue";
-canvasBG.style.position = "absolute";
-canvasBG.style.top = "0";
-canvasBG.style.left = "0";
+canvasBG.style.position = "absolute"
+canvasBG.style.top = "0"
+canvasBG.style.left = "0"
 canvasBG.style.transform = "rotateX(180deg)"
 document.body.appendChild(canvasBG)
 
 /*** P5 RUNTIME ***/
-let mainPG, img1, img2, bgImage1, bgImage2, glitchShader1, glitchShader2, glitchShader;
+let mainPG, img1, img2, bgImage1, bgImage2, glitchShader
 
 function preload() {
   img1 = loadImage("./img-1.png")
   img2 = loadImage("./img-2.png")
   glitchShader = loadShader("./glitch.vert", "./glitch.frag")
-  // glitchShader1 = loadShader('./glitch.vert', './glitch.frag')
-  // glitchShader2 = loadShader('./glitch.vert', './glitch.frag')
 }
 
 function setup() {
@@ -28,7 +25,6 @@ function setup() {
   mainPG = createGraphics(w, h)
   bgImage1 = new BGImage({
     img: img1,
-    // shader: glitchShader1,
     xpos: "left",
     ypos: "top",
     ratio: 0.62,
@@ -38,7 +34,6 @@ function setup() {
 
   bgImage2 = new BGImage({
     img: img2,
-    // shader: glitchShader2,
     xpos: "right",
     ypos: "bottom",
     ymargin: 80,
@@ -56,8 +51,7 @@ function windowResized() {
 
 function draw() {
   mainPG.clear()
-  // clear()
-  background(0)
+  clear()
   shader(glitchShader)
   glitchShader.setUniform("iResolution", [width, height]);
   glitchShader.setUniform("iFrame", frameCount);
@@ -66,8 +60,6 @@ function draw() {
   glitchShader.setUniform("iChannel0", mainPG);
   bgImage1.render()
   bgImage2.render()
-
-  // image(mainPG, -width / 2, -height / 2, width, height)
   rect(0, 0, width, height)
 }
 
@@ -85,7 +77,6 @@ class BGImage {
   }) {
     this.image = img
     this.shader = shader
-    // this.pg = createGraphics(img.width, img.height)
     this.xpos = xpos
     this.ypos = ypos
     this.xmargin = xmargin
@@ -96,7 +87,6 @@ class BGImage {
   }
 
   render() {
-
     //calculate render proportions
     const w = width * this.ratio;
     const rw = w < this.minWidth ? this.minWidth : w > this.maxWidth ? this.maxWidth : w
@@ -107,21 +97,7 @@ class BGImage {
     const y = this.ypos === "top" ? 0 + this.ymargin :
       this.ypos === "bottom" ? height - rh - this.ymargin :
         width / 2
-
-    // apply shader with graphics buffer as channel
-    // shader(this.shader)
-    // this.shader.setUniform("iResolution", [rw, rh]);
-    // this.shader.setUniform("iFrame", frameCount);
-    // this.shader.setUniform("iMouse", [mouseX, mouseY]);
-    // this.shader.setUniform("iTime", frameCount * 0.000001);
-    // this.shader.setUniform("iChannel0", this.pg);
-
-    // this.pg.clear()
-    // this.pg.image(this.image, 0, 0, this.image.width, this.image.height)
-
+    //render the image in graphics buffer
     mainPG.image(this.image, x, y, rw, rh)
-
-    // image(this.pg, x, y, rw, rh)
-    // rect(x, y, rw, rh)
   }
 }
